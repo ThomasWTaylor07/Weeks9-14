3f0ef2ece188cee05e8457aec64e449f58587827
taythoma <taythoma@OA-S141-24>
2026-03-25 11:54:52 -0400

    Completed the footsteps challenge and a majority of the pulse challenge.

--- Assets/Scripts/Knight.cs
+++ Assets/Scripts/Knight.cs
@@ -4,22 +4,47 @@ using UnityEngine;
 public class Knight : MonoBehaviour
 {
     public AudioSource AudioSource;
-   
+    public AudioClip Clip1;
+    public AudioClip Clip2;
+    public AudioClip Clip3;
+    public AudioClip Clip4;
+    public float clipValue;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-      
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void footStep()
     {
-        AudioSource.Play();
+        clipValue = Random.Range(1, 5);
+
+        if (clipValue == 1)
+        {
+            AudioSource.clip = Clip1;
+            AudioSource.Play();
+        }
+        if (clipValue == 2)
+        {
+            AudioSource.clip = Clip2;
+            AudioSource.Play();
+        }
+        if (clipValue == 3)
+        {
+           AudioSource.clip = Clip3;
+            AudioSource.Play();
+        }
+        if (clipValue == 4)
+        {
+            AudioSource.clip = Clip4;
+            AudioSource.Play();
+        }
         Debug.Log("Step!");
     }
 }
---
+++ Assets/Scripts/TrailMove.cs
@@ -0,0 +1,50 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class TrailMove : MonoBehaviour
+{
+    public float speedX = 0.01f;
+    public float speedY = 1;
+    public AnimationCurve ac;
+    public float t;
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime / 2;
+        if (t > 1)
+        {
+
+
+            t = 0;
+
+        }
+
+
+        Debug.Log(speedY * ac.Evaluate(t));
+        Vector2 newPos = transform.position;
+
+        newPos.x += 1 * Time.deltaTime;
+        newPos.y = ac.Evaluate(t);
+        transform.position = newPos;
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+        if (screenPos.x >= Screen.width + 100)
+        {
+          
+            newPos.x = -12;
+            transform.position = newPos;
+        }
+
+        else
+        {
+
+        }
+        }
+    }
