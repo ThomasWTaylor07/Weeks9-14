9afc4c621145c79f6a994b7c3cb07d93eb70430b
SHERNET\taythoma <taythoma@sheridancollege.ca>
2026-04-06 14:07:32 -0400

    Started addding comments.

--- Assets/System Scripts/Coin Script.cs
+++ Assets/System Scripts/Coin Script.cs
@@ -2,26 +2,37 @@ using UnityEngine;
 
 public class CoinScript : MonoBehaviour
 {
+    //Initializes a variable that gets access to the player objects sprite renderer so that it can recognize whever the players sprite renderer is within bounds of a coin
     public SpriteRenderer player;
+    //Initializes a float that gets used to determine the objects x position
     public float xPos;
+    //Initializes a float that gets used to determine the objects y position
     public float yPos;
+    //Initializes a boolean that gets set to true whenever the coins are within the playesr sprite renderer so that the coin can be destroyed by the coin spawner script
     public bool collected;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //Sets the coins x position variable to a random value so that when it's instantiated, it appears in a random x position
         xPos = Random.Range(-8, 8);
+        //Sets the coins y position variable to a random value so that when it's instantiated, it appears in a random x position
         yPos = Random.Range(-5, 5);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Assigns a new Vector2 to the objects transform that will take in values and use them to change where the coins are on screen
         Vector2 newPos = transform.position;
+        //Sets the values of newPos to the xPos and yPos variables so that the objects transform can change the objects position using them
         newPos.x = xPos;
         newPos.y = yPos;
+        //Assigns the obejcts transform to newPos so that it can take its values and use them to change the objects position
         transform.position = newPos;
+        //Creates an if statement so that whenever a coin is within the players sprite renderer, something happens so it can be collected
         if (player.bounds.Contains(transform.position))
         {
+        //Sets boolean to true to show that a coin has been collected which is used by the coin spawner script to see when a coin should be destroyed
             collected = true;
         }
     }
--- Assets/System Scripts/PlayerMovement.cs
+++ Assets/System Scripts/PlayerMovement.cs
@@ -4,10 +4,10 @@ using UnityEngine.Rendering.Universal;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //Sets a speed variable that will be used to allow the player to move across the screen whenever the input system gives a non 0 value
     public float speed = 6;
-    public Vector2 playerMovement;
-    public AudioSource source;
-    public AudioClip clip;
+    //Creates a Vector3 that contains the value given to it by the Unity input system so that it can move the objects transform
+    public Vector3 playerMovement;
   
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,24 +18,23 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        //Assigns a new Vector3 to the objeccts transform that will get assigned a new value and change the transform
         Vector3 newPos = transform.position; 
-        newPos += (Vector3)playerMovement * speed * Time.deltaTime;
+        //Sets the vector3 to the values given by the Unity input system multiplied by speed and delta time so that the players inputs can change the objects transform
+        newPos += playerMovement * speed * Time.deltaTime;
+        //Puts a clamp on the newPos x and y values by setting the minimum and maximum value they are allowed to be
         newPos.x = Mathf.Clamp(newPos.x, -9, 9);
+        //This means that the object will never appear fully off screen
         newPos.y = Mathf.Clamp(newPos.y, -5, 5);
+        //Assigns newPos back to the objects transform so that it can use the updated values given to newPos to move the object 
         transform.position = newPos;
     }
-
+    //Creates a function with a paramater that gives us access to the value from the input system
     public void OnMove(InputAction.CallbackContext context)
     {
+        //The player movement variable gets assigned the value from the input system as a Vector2 so that player inputs can change its value and move the object
         playerMovement = context.ReadValue<Vector2>();
     }
 
-    public void collide()
-    {
-       
-        
-       
-    }
     
 }
