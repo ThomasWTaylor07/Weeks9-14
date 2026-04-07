c53259b5254226d01ee7c8350342ec847ef56a22
SHERNET\taythoma <taythoma@sheridancollege.ca>
2026-04-07 19:45:26 -0400

    Made slight alterations to the player movement script.

--- Assets/System Scripts/PlayerMovement.cs
+++ Assets/System Scripts/PlayerMovement.cs
@@ -33,9 +33,9 @@ public class PlayerMovement : MonoBehaviour
     public void OnMove(InputAction.CallbackContext context)
     {
         {
-
-            //The player movement variable gets assigned the value from the input system as a Vector2 so that player inputs can change its value and move the object
-            playerMovement = context.ReadValue<Vector2>();
+                //The player movement variable gets assigned the value from the input system as a Vector2 so that player inputs can change its value and move the object
+                playerMovement = context.ReadValue<Vector2>();
+            
         }
     }
     
